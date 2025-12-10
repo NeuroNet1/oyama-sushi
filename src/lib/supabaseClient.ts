98cfffdc207e55335f2eb5bf696628656c7edad3
Kaitovid <bejaranodavid08@gmail.com>
2025-12-10 16:23:21 -0500

    V 5.0

--- src/lib/supabaseClient.ts
+++ src/lib/supabaseClient.ts
@@ -1,7 +1,6 @@
 import { createClient } from '@supabase/supabase-js';
 
-const SUPABASE_URL = 'https://uygkzfevnqeiptexlfhe.supabase.co';
-const SUPABASE_KEY = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InV5Z2t6ZmV2bnFlaXB0ZXhsZmhlIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NjQ2MTMzNjIsImV4cCI6MjA4MDE4OTM2Mn0.nKno6JYzBf8jk5Bv7Uu1iEnEUXKy5o4U875yObUfTj4';
-
+const SUPABASE_URL = "https://aftnevcikbxuxankkybh.supabase.co";
+const SUPABASE_KEY = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImFmdG5ldmNpa2J4dXhhbmtreWJoIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NjQ1OTU1MjQsImV4cCI6MjA4MDE3MTUyNH0.SSf1bLw138F9MaR0qQKFuvs097N1cTSiSQSU_sXe4I8";
 export const supabase = createClient(SUPABASE_URL, SUPABASE_KEY);
 export default supabase;
